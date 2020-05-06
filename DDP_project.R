4c2980217ba5d49fe31fc69a4bff7d6b0c423970
JihanYu <styjh80@gmail.com>
2020-05-06 17:57:20 +0900

    modify project

--- DDP_project.R
+++ DDP_project.R
@@ -36,37 +36,60 @@ ggplot(df.a, aes(x=x, y=y1)) +
 				aes(ymin=0, ymax=y1), fill="blue", alpha=0.5) 
 
 
+library(ggplot2)
+
 a <- 0.05;  b <- 0.2
-effect.size <- 5
+effect.size <- 0.5
 mu0 <- 0;  std.drv0 <- 1
 mu1 <- mu0 + effect.size;  std.drv1 <- 1
 one.two <- TRUE
-n1 <- 100;  n1.n0 <- 1;  
+n1 <- 130;  n1.n0 <- 1;  
+two.sided <- 1;  if(one.two == TRUE) two.sided <- 2
 
-if(is.na(n1)){
-  
+za <- qnorm(a/two.sided, lower.tail=FALSE)
+zb <- qnorm(b, lower.tail=FALSE)
+
+q12 <- NULL
+if(n1.n0 >= 1){ 
+	q12 <- 1 + 1/n1.n0
+} else {
+	q12 <- 1 + n1.n0
 }
 
+if(is.na(n1)){
+	n1 <- ( (za + zb)^2 * std.drv0^2 * (q12)^2 ) / (effect.size^2)
+}
 n0 <- n1/n1.n0
+
 std.err0 <- std.drv0 / sqrt(n0/2)
 std.err1 <- std.drv1 / sqrt(n1/2)
-
-two.sided <- 1;  if(one.two == TRUE) two.sided <- 2
-x.inter <- qnorm(a/two.sided, lower.tail=FALSE)
 
 x <- seq(from = mu0 - 5*std.err0, to = mu1 + 5*std.err1, by=0.01)
 h0 <- dnorm(x, mean=mu0, sd=std.err0)
 h1 <- dnorm(x, mean=mu1, sd=std.err1)
 h.norm <- data.frame(x, h0, h1)
 
+a.h0 <- qnorm(a/two.sided, mean=mu0, sd=std.err0, lower.tail=FALSE)
+b.h1 <- qnorm(b, mean=mu1, sd=std.err1)
+
 p <- ggplot(h.norm, aes(x=x, y=h0)) +
 	geom_line(aes(x=x, y=h0), color="red") +
 	geom_line(aes(x=x, y=h1), color="blue") +
-	geom_vline(xintercept=x.inter) +
-	geom_ribbon(data=subset(h.norm, x.inter <= x & x <= max(x)), 
+	geom_vline(xintercept=a.h0) +
+	geom_vline(xintercept=b.h1) +
+	geom_ribbon(data=subset(h.norm, a.h0 <= x & x <= max(x)), 
 				aes(ymin=0, ymax=h0, fill="H0", alpha=0.5)) +
-	geom_ribbon(data=subset(h.norm, min(x) <= x & x <= x.inter),
+	geom_ribbon(data=subset(h.norm, min(x) <= x & x <= a.h0),
 				aes(ymin=0, ymax=h1, fill="H1", alpha=0.5))
 print(p)
 
-pnorm(qnorm(0.05), lower.tail=FALSE)
+power.b <- pnorm(qnorm(a/two.sided, lower.tail=FALSE), mean=mu1, sd=std.err1, lower.tail=FALSE)
+
+if (power.b >= (1-b)){
+	cat("Power :", power.b, "- adequate\n")
+} else{
+	cat("Power :", power.b, "- inadequate\n")
+}
+
+
+x.inter <- qnorm(a/two.sided, lower.tail=FALSE)
